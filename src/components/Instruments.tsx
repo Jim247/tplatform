ddbc8ce6008d1116eefe07f7463ae1aa5d762730
Jim Burgess <jim@tempomobile.co.uk>
2025-07-29 14:23:25 +0100

    feat: Add additional instruments to Instruments component and update layout for better display

--- src/components/Instruments.tsx
+++ src/components/Instruments.tsx
@@ -3,11 +3,15 @@ import {
   mdiGuitarAcoustic, 
   mdiGuitarElectric, 
   mdiMusicClefBass,
-  mdiMicrophoneVariant 
+  mdiMicrophoneVariant, 
+  mdiPiano,
+  mdiHeadDotsHorizontal,
+  mdiMore,
+  mdiDotsHorizontal,
+  mdiDotsHorizontalCircleOutline
 } from "@mdi/js";
 
 export default function Instruments() {
-  // Smaller icon and text for mobile, larger for md+
   const iconSize = 4
 
   return (
@@ -35,6 +39,15 @@ export default function Instruments() {
           />
           <span className="mt-2 text-sm md:text-xl text-white font-semibold text-center">Electric Guitar</span>
         </div>
+          <div className="flex flex-col items-center group cursor-pointer p-2">
+          <Icon 
+            path={mdiPiano} 
+            size={iconSize} 
+            color="var(--highlight)"
+            className="transition-transform duration-200 group-hover:scale-110 group-hover:-translate-y-1"
+          />
+          <span className="mt-2 text-sm md:text-xl text-white font-semibold text-center">Piano/Keyboard</span>
+        </div>
         <div className="flex flex-col items-center group cursor-pointer p-2">
           <Icon 
             path={mdiMusicClefBass} 
@@ -52,6 +65,15 @@ export default function Instruments() {
             className="transition-transform duration-200 group-hover:scale-110 group-hover:-translate-y-1"
           />
           <span className="mt-2 text-sm md:text-xl text-white font-semibold text-center">Singing</span>
+        </div>
+         <div className="flex flex-col items-center group cursor-pointer p-2">
+          <Icon 
+            path={mdiDotsHorizontalCircleOutline} 
+            size={iconSize} 
+            color="var(--highlight)"
+            className="transition-transform duration-200 group-hover:scale-110 group-hover:-translate-y-1"
+          />
+          <span className="mt-2 text-sm md:text-xl text-white font-semibold text-center">...And More</span>
         </div>
       </div>
     </div>
